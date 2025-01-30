f0c62198f7b91907e088710fb45fbce3a63f48a0
mutebi paul <engpaulmutebi@gmail.com>
2025-01-30 09:18:12 +0300

    first commit

--- myblog/urls.py
+++ myblog/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include # type: ignore
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include("blogApp.urls")),
+    path('', include("blogApp.urls")),
 
 ]
